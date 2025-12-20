84882168a8b6d6104ebe4171c04bf6de176e645b
Belldevs <115190907+Belldevs@users.noreply.github.com>
2025-12-20 15:07:34 +0100

    Add HomeSections component to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,20 +3,22 @@
 <Hero/>
 <About/>
 <TeamCard/>
-
+<HomeSections/>
 <div></div>
    
 </template>
 <script>
 import Hero from '@/assets/components/Hero.vue';
 import About from '@/assets/components/About.vue';
+import HomeSections from '@/assets/components/VideoCard.vue';
 import TeamCard from '@/assets/components/TeamCard.vue';
 export default {
-    components:{ Hero , About ,TeamCard}
+    components:{ Hero , About ,TeamCard, HomeSections}
 }
 </script>
 <style scoped>
     h1{
         margin-top:200px;
     }
+
 </style>
